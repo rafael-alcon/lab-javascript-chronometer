0b1728e907c4a6b3d2e74fe3665666a935e7e2cd
Rafael Alcon <rafa_prog@hotmail.com>
2025-04-11 11:32:34 -0400

    logic part completed

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,39 +1,67 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime= 0
+    this.intervalId=null
   }
 
   start(callback) {
-    // ... your code goes here
+    // ... your code goes here    
+      this.intervalId= setInterval((callback)=>{
+        this.currentTime+=1
+        //console.log(this.currentTime)
+      },1000)
+    
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
     // ... your code goes here
+    return (this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if(value>=0 && value<10){
+      return "0" + value
+    }else{
+      return String(value)
+    }
+
   }
 
   stop() {
-    // ... your code goes here
+    // ... your code goes here()
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime=0
   }
 
   split() {
     // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds())
   }
 }
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
+
 if (typeof module !== 'undefined') {
   module.exports = Chronometer;
 }
+  
+
+let reloj= new Chronometer
+reloj.computeTwoDigitNumber(4)
+reloj.computeTwoDigitNumber(0)
+reloj.computeTwoDigitNumber(15)
+
+
+
